0f2d9df708fe6394e4dce2b3ed49a23cdbb470f2
bautiPezM <bpaezmoll139@gmail.com>
2026-09-14 14:33:42 -0300

    Refactor gestion.h with updated function signatures

--- gestion.h
+++ gestion.h
@@ -12,9 +12,10 @@ struct Reservas {
 
 };
 
-void altareservas(std::vector<reservas>& lista);
-void listarreservas(const std::vector<reservas>& lista);
-void modificarSocio(std::vector<reservas>& lista);
-void bajaSocio(std::vector<reservas>& lista);
-int guardarArchivo(const std::vector<reservas>& lista);
-int leerArchivo(std::vector<reservas>& lista);
+int altareservas(); 
+int listaReservas(); //Consulta general de todas las reservas
+int busquedaID(); //Busqueda de una reserva por ID
+int confirmarReserva(); //Confirmacion de una reserva existente
+int modificarReserva(); //Modificacion de una reserva existente
+int bajaReserva(); //Baja lógica de una reserva existente
+int guardarArchivo(); //Guardar la lista de reservas en un archivo binario
