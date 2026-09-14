b5d2cb488690c1c8571e67f439cf6b144a673ce6
bautiPezM <bpaezmoll139@gmail.com>
2026-09-14 14:06:29 -0300

    Fix struct Reservas and add function declarations
    Added function declarations for managing reservations.

--- gestion.h
+++ gestion.h
@@ -3,11 +3,18 @@
 #include <string>
 #include <vector>
 struct Reservas {
-	int idReserva
-		char Responsable[50]
-		char Aula[50]
-		int CantidadPersonas
-		bool confirmada
-		bool Activo
+		int idReserva;
+		char Responsable[50];
+		char Aula[50];
+		int CantidadPersonas;
+		bool confirmada;
+		bool Activo;
 
 };
+
+void altareservas(std::vector<reservas>& lista);
+void listarreservas(const std::vector<reservas>& lista);
+void modificarSocio(std::vector<reservas>& lista);
+void bajaSocio(std::vector<reservas>& lista);
+int guardarArchivo(const std::vector<reservas>& lista);
+int leerArchivo(std::vector<reservas>& lista);
