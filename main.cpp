8603421c01f2fdc7514794ebd3783418cbee222f
Lara Agostina Figueroa <larfigueroa@ubp.edu.ar>
2026-09-16 16:48:05 -0300

    main corregido

--- main.cpp
+++ main.cpp
@@ -1,41 +1,53 @@
-do {
-    char opcion = ' ';
-    cout << "CASO 3: RESERVA DE AULAS Y LABORATORIO" << endl;
-    cout << "Seleccione una opcion: " << endl;
-    cout << "Alta de reservas" << endl;
-    cout << "Consulta general" << endl;
-    cout << "Busqueda por ID" << endl;
-    cout << "Modificacion" << endl;
-    cout << "Confirmar reserva" << endl;
-    cout << "Baja logica" << endl;
-    cin >> opcion;
-    switch (opcion) {
-    case '1': {
+#include<iostream>
+#include "gestion.h"
+
+using namespace std;
+
+int main() {
 
 
-    }break;
-    case '2': {
+    do {
+        char opcion = ' ';
+        cout << "CASO 3: RESERVA DE AULAS Y LABORATORIO" << endl;
+        cout << "Seleccione una opcion: " << endl;
+        cout << "Alta de reservas" << endl;
+        cout << "Consulta general" << endl;
+        cout << "Busqueda por ID" << endl;
+        cout << "Modificacion" << endl;
+        cout << "Confirmar reserva" << endl;
+        cout << "Baja logica" << endl;
+        cin >> opcion;
+        switch (opcion) {
+        case '1': {
 
 
-    }break;
-    case '3': {
+        }break;
+        case '2': {
 
 
-    }break;
-    case '4': {
+        }break;
+        case '3': {
 
 
-    }break;
-    case '5': {
+        }break;
+        case '4': {
 
 
-    }break;
-    case '6': {
+        }break;
+        case '5': {
 
 
-    }break;
+        }break;
+        case '6': {
 
-    }
+
+        }break;
+
+        }
+
+    } while (opcion != '7');
+
+
 
 }
 
