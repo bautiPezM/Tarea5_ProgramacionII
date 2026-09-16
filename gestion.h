7adb8efd3d25b5eb8ce4d6bc9be018dc437b8598
Mateo Allocco Pasqualis <malloccopasqualis@ubp.edu.ar>
2026-09-16 12:18:04 -0300

    putas main lechosas Update README.md

--- gestion.h
+++ gestion.h
@@ -1,2 +1,20 @@
 #pragma once
-struct
+#include <vector>
+
+
+namespace calculo {
+
+    struct reservas {
+        char nombre[50];
+        char apellido[50];
+        int edad;
+        char deporte[50];
+    };
+    void altareservas(std::vector<reservas>& lista);
+    void listarreservas(const std::vector<reservas>& lista);
+    void modificarSocio(std::vector<reservas>& lista);
+    void bajaSocio(std::vector<reservas>& lista);
+    int guardarArchivo(const std::vector<reservas>& lista);
+    int leerArchivo(std::vector<reservas>& lista);
+
+}
