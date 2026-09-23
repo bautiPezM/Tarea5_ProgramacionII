e3388c1bc9582297deb17919deeed1e828eff003
Mateo Allocco Pasqualis <malloccopasqualis@ubp.edu.ar>
2026-09-23 12:29:24 -0300

    gestion.h y main corregidas

--- gestion.h
+++ gestion.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 
+
 int altareservas();
 int listaReservas(); //Consulta general de todas las reservas
 int busquedaID(); //Busqueda de una reserva por ID
@@ -12,3 +13,4 @@ int confirmarReserva(); //Confirmacion de una reserva existente
 int modificarReserva(); //Modificacion de una reserva existente
 int bajaReserva(); //Baja lógica de una reserva existente
 int guardarArchivo(); //Guardar la lista de reservas en un archivo binario
+int consultaEspecial();//Consulta especial del caso
--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ int main() {
         cout << "4. Modificacion" << endl;
         cout << "5. Confirmar reserva" << endl;
         cout << "6. Baja logica" << endl;
-        cout << "7. Salir" << endl;
+        cout << "7. Consulta especial" << endl;
+        cout << "8. Salir" << endl;
         cout << "Seleccione una opcion: ";
         cin >> opcion;
         switch (opcion) {
@@ -24,53 +25,76 @@ int main() {
             int valor = altareservas();
             if (valor == -1) cout << "ocurrio un error" << endl;
             cout << endl;
-
-        }break;
+            system("pause");
+            break;
+        }
         case '2': {
             cout << endl;
             int valor = listaReservas();
             if (valor == -1) cout << "ocurrio un error" << endl;
             if (valor == 1) cout << "se realizo con exito" << endl;
             cout << endl;
-
-        }break;
+            system("pause");
+            break;
+        }
         case '3': {
             cout << endl;
             int valor = busquedaID();
             if (valor == -1) cout << "ocurrio un error" << endl;
             else  cout << "se realizo con exito" << endl;
-
-        }break;
+            system("pause");
+            break;
+        }
         case '4': {
             cout << endl;
             int valor = modificarReserva();
             if (valor == -1) cout << "ocurrio un error" << endl;
             if (valor == 0) cout << "se realizo con exito" << endl;
             cout << endl;
-
-        }break;
+            system("pause");
+            break;
+        }
         case '5': {
             cout << endl;
             int valor = confirmarReserva();
             if (valor == -1) cout << "ocurrio un error" << endl;
             if (valor == 0) cout << "se realizo con exito" << endl;
             cout << endl;
-
-        }break;
+            system("pause");
+            break;
+        }
         case '6': {
-
             cout << endl;
             int valor = bajaReserva();
             if (valor == -1) cout << "ocurrio un error" << endl;
             if (valor == 0) cout << "se realizo con exito" << endl;
             cout << endl;
-        }break;
-
+            system("pause");
+            break;
         }
-
-    } while (opcion != '7');
+        case '7': {
+            cout << endl;
+            int valor = consultaEspecial();
+            if (valor == -1) cout << "ocurrio un error" << endl;
+            if (valor == 0) cout << "se realizo con exito" << endl;
+            cout << endl;
+            system("pause");
+            break;
+        }
+        case '8': {
+            cout << "Saliendo del programa..." << endl;
+            system("pause");
+            break;
+        }
+        default: {
+            cout << "Opcion invalida. Intente de nuevo" << endl;
+            system("pause");
+            break;
+        }
+        }
+        system("cls");
+    } while (opcion != '8');
 
     return 0;
 
 }
-
